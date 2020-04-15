bdc7abed4c08022e450bd4440c53bba92bb79602
AlexandraSuciu26 <62299115+AlexandraSuciu26@users.noreply.github.com>
2020-04-15 15:08:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,24 +10,28 @@ import matplotlib.pyplot as plt
 
 
 data = ReadData("C:\\@Alexandra\\anul2\\semestrul2\\ai\\lab\\laborator4\\berlin.txt")
-params = {'popSize': 5, 'noGen': 1000}
+params = {'popSize': 100, 'noGen': 200}
 ga = GA(params, data.problParams)
 ga.initialisation()
 ga.evaluation()
 
 res=[]
+res1=[]
 for i in range(params['noGen']):
-    ga.oneGeneration()
+    #ga.oneGeneration()
     #ga.oneGenerationElitism()
-    #ga.oneGenerationSteadyState()
+    ga.oneGenerationSteadyState()
     best = ga.bestChromosome()
     fitnesses = [c.fitness for c in ga.population]
     avgFitness = sum(fitnesses) / len(fitnesses)
     res.append(avgFitness)
+    for c in ga.population:
+        res1.append(c.fitness)
+        print("Fiteness:"+str(c.fitness)+"\n")
     print('Generation: ' + str(i) + '\nBest chromosome: ' + str(best.repres) + '\nLocal best fitness: ' + str(best.fitness)
-         +'\nAverage fitness: '+ str(avgFitness)+'\n')
+         +'\nAverage fitness: '+ str(fitnesses)+'\n')
     
-plt.plot(res)
+plt.plot(res1)
 plt.ylabel('Best Fit/Generation')
 plt.xlabel('Generation')
 plt.show()
